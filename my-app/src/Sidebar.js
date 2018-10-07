f6c238b14e53213f5719200c09b22de3a003ad36
Gokulan Gnanendran <gxg5186@psu.edu>
2018-10-07 09:28:11 -0700

    added some more csss

--- my-app/src/Sidebar.js
+++ my-app/src/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Table from 'rc-table'
-import 'rc-table/assets/index.css';
+
 
 const columns = [{
     title: 'Address', dataIndex: 'address', key:'address', width: 200,
@@ -24,17 +24,13 @@ class Sidebar extends Component {
     const doubleSpend = this.props.doubleSpend
     const data = (clickedNode) ?
       Object.keys(clickedNode.state).map(nodeId => { return {...clickedNode.state[nodeId], address: nodeId.substring(0,10), key: nodeId}}) : null
-    const getInvalidNonceTxs = (address) => {
-      if (clickedNode.invalidNonceTxs && clickedNode.invalidNonceTxs[address]){
-        return JSON.stringify(clickedNode.invalidNonceTxs[address])
-      } else return 'None'
-    }
+      console.log(data)
     //<p>{'Node ' + clickedNode.pid + '\n State:' + JSON.stringify(clickedNode.state) + '\n Invalid Nonce Txs:' +  JSON.stringify(clickedNode.invalidNonceTxs)
     return (
-        <div id = "Node-state">
+        <div id = "Sidebar">
           {clickedNode ? (
             <div>
-              <Table columns= {columns} data = {data} expandedRowRender={record => {return 'Invalid Nonce Txs: ' + getInvalidNonceTxs(record.key)}} expandIconAsCell/>
+              <Table columns= {columns} data = {data} />
               <button style={{background:"red"}} onClick={()=>doubleSpend(clickedNode)}>Double Spend</button>
             </div>) :
             'No node selected.'
