779f98fa84bf4a13a885b629395918fd08618ff6
Gokulan Gnanendran <gxg5186@psu.edu>
2018-10-07 08:53:43 -0700

    added some more csss

--- my-app/src/react-d3-graph-custom/src/components/graph/graph.config.js
+++ my-app/src/react-d3-graph-custom/src/components/graph/graph.config.js
@@ -129,7 +129,7 @@
  */
 export default {
     automaticRearrangeAfterDropNode: false,
-    height: 700,
+    height: 800,
     highlightDegree: 1,
     highlightOpacity: 1,
     linkHighlightBehavior: false,
@@ -138,7 +138,7 @@ export default {
     nodeHighlightBehavior: false,
     panAndZoom: false,
     staticGraph: false,
-    width: 1000,
+    width: 1300,
     node: {
         color: '#d3d3d3',
         fontColor: 'black',
