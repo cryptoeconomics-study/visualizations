5358049a6cd089bf754cc56cf0dd9a6ab05cfbc2
Kevin Ho <kevinjho1996@gmail.com>
2018-10-21 17:42:44 -0700

    changed text

--- my-app/src/components/Network.js
+++ my-app/src/components/Network.js
@@ -274,22 +274,22 @@ class Network extends Component {
     const {clickedNode, selectedNodes, messages, time, paused, pausedTxs, speed} = this.state
     return (
       <div id="App-container">
-{/*        <div id="Text-container">
-        <div id="Overflow-top"></div>
-        <h3>V2: A Distributed Ledger</h3><div id="Text">Building a centralized payments processor like Paypal is simple, but relies on trust that Paypal will not break the rules. A simple way to decentralize PayPal is to make clients download all transactions and run the PayPal code to generate their belief of the current state.  In this section we see why this is not enough, how to mentally model a network & synchrony assumptions.<br/><br/>
+        <div id="Text-container">
+          <div id="Overflow-top"></div>
+          <h3>2.2: The Double Spend</h3><div id="Text">Building a centralized payments processor like Paypal is simple, but relies on trust that Paypal will not break the rules. A simple way to decentralize PayPal is to make clients download all transactions and run the PayPal code to generate their belief of the current state. {/* In this section we see why this is not enough, how to mentally model a network & synchrony assumptions.<br/><br/>
 
-<b>Synchronous network</b><br/>Global clock, & there is a known (constant) latency L in which all messages are assumed to be received. For instance all messages propagate in 5 seconds.<br/><br/>
+  <b>Synchronous network</b><br/>Global clock, & there is a known (constant) latency L in which all messages are assumed to be received. For instance all messages propagate in 5 seconds.<br/><br/>
 
-<b>Partially Synchronous network</b><br/>There is some unknown latency L in which all messages are assumed to be received. It is important to note that this latency is unknown and could be extremely high.<br/><br/>
+  <b>Partially Synchronous network</b><br/>There is some unknown latency L in which all messages are assumed to be received. It is important to note that this latency is unknown and could be extremely high.<br/><br/>
 
-<b>Asynchronous network</b><br/>Local clock, & there are no timing assumptions made. We are not able to determine objectively the time ordering of transactions, though each individual node still has an idea of what order it saw messages arrive in (and different nodes can disagree).<br/><br/>
+  <b>Asynchronous network</b><br/>Local clock, & there are no timing assumptions made. We are not able to determine objectively the time ordering of transactions, though each individual node still has an idea of what order it saw messages arrive in (and different nodes can disagree). */}<br/><br/>
 
-In a decentralized system, we cannot rely on a global clock, and we cannot assume a constant latency for all messages to be delivered.<br/><br/>
+ In a decentralized system, we cannot rely on a global clock, and we cannot assume a constant latency for all messages to be delivered.<br/><br/>
 
-This is the root cause of the double spend problem: an attacker can send one message to Jing & another message to Aparna each spending the same coins. If Jing and Aparna both accept those transactions, their states will diverge and we will have a fork. Not good! We need decentralized consensus!
-          </div>
-        <div id="Overflow-bottom"></div>
-        </div>*/}
+  This is the root cause of the double spend problem: an attacker can send one message to Jing & another message to Karl each spending the same coins. If Jing and Karl both accept those transactions, their states will diverge and we will have a fork. Not good! We need decentralized consensus!
+            </div>
+          <div id="Overflow-bottom"></div>
+        </div>
         <div id = "Network-container">
           <div id = "Graph-container">
             <Ledgers
