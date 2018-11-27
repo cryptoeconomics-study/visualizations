9c5fe93d104926fde46bd1831adf644bc5fe8f9f
Kevin Ho <kevinjho1996@gmail.com>
2018-11-27 15:27:59 -0800

    turned Tray into functional stateless component

--- my-app/src/components/Tray.jsx
+++ my-app/src/components/Tray.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import TrayButton from './TrayButton.jsx'
 
-class Tray extends Component {
-  render() {
-    const {nodes, selectedNodes, icons, showState} = this.props
-
-    return (
-        <div id="Tray">
-        {
-          Object.keys(nodes).map((pid, index) => (
-        		<TrayButton key={pid} isSelected={(nodes[pid].pid in selectedNodes)} icons={icons} node={nodes[pid]} showState={showState}/>
-          ))
-		}
-        </div>
-    );
-  }
+const Tray = ({nodes, selectedNodes, icons, showState}) => {
+  return (
+    <div id="Tray">
+      {Object.keys(nodes).map((pid, index) =>
+        <TrayButton
+          key={pid}
+          isSelected={(nodes[pid].pid in selectedNodes)}
+          icons={icons}
+          node={nodes[pid]}
+          showState={showState}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Tray;
