25c03413f701e383266d6a806b2da12e5fac7a5b
Kevin Ho <kevinjho1996@gmail.com>
2018-11-27 15:18:46 -0800

    turned Controls into a functional stateless component👻

--- my-app/src/components/Controls.js
+++ my-app/src/components/Controls.js
@@ -2,27 +2,22 @@ import React, { Component } from 'react';
 import { Button, Glyphicon, Well} from 'react-bootstrap';
 import Switch from 'rc-switch';
 
-
-class Controls extends Component {
-  render() {
-    const {paused, onPause, onPauseTxs, pausedTxs, togglePopup} = this.props
-
-    return (
-        <div id = "Controls">
-          <Button onClick={onPause}>{paused ?
-            <Glyphicon glyph="play" />
-           : <Glyphicon glyph="pause" />}</Button>
-          <div id="txToggle">
-          Send Transactions <Switch
-             onChange={onPauseTxs}
-           />
-           </div>
-          <Button onClick={togglePopup}>
-            <Glyphicon glyph="question-sign" />
-          </Button>
-        </div>
-    );
-  }
+const Controls = ({paused, onPause, onPauseTxs, pausedTxs, togglePopup}) => {
+  return (
+      <div id = "Controls">
+        <Button onClick={onPause}>{paused ?
+          <Glyphicon glyph="play" />
+         : <Glyphicon glyph="pause" />}</Button>
+        <div id="txToggle">
+        Send Transactions <Switch
+           onChange={onPauseTxs}
+         />
+         </div>
+        <Button onClick={togglePopup}>
+          <Glyphicon glyph="question-sign" />
+        </Button>
+      </div>
+  );
 }
 
 export default Controls;
