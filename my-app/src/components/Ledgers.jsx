64bad2d068d6d7a7983fe91239a525b1085dbf49
Kevin Ho <kevinjho1996@gmail.com>
2018-11-27 15:33:19 -0800

    ledgers -> functional stateless component

--- my-app/src/components/Ledgers.jsx
+++ my-app/src/components/Ledgers.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import Ledger from './Ledger.jsx'
 
-
-class Ledgers extends Component {
-  render() {
-    const {nodes, icons, showState} = this.props
-
-    return (
-        <div id = "Ledgers">
-        {
-        	Object.keys(nodes).map((pid, index) => (
-        		<Ledger key={pid} node={nodes[pid]} icons={icons} showState={showState}/>
-        	))
-        }
-        </div>
-    );
-  }
+const Ledgers = ({nodes, icons, showState}) => {
+  return (
+    <div id="Ledgers">
+      {Object.keys(nodes).map((pid, index) =>
+        <Ledger
+          key={pid}
+          node={nodes[pid]}
+          icons={icons}
+          showState={showState}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Ledgers;
